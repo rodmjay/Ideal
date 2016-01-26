7768dc8db0d37ad2c8e159cd0f98d7b72f6544ba
Rod Johnson <rodmjay@wcpro.com>
2016-01-25 21:53:32 -0700

    updated nuget packagees

--- Ideal/app/accounts.js
+++ Ideal/app/accounts.js
@@ -1,1 +1,14 @@
-﻿angular.module('ideal.accounts', ['ideal.common']);
+﻿angular
+    .module('ideal.accounts', ['ideal.common'])
+    .directive('iLoginForm',[function() {
+        return {
+            templateUrl: '/accounts/login',
+            restrict: 'E',
+            link:function(scope, elem, attrs, ctrl) {
+                
+            },
+            controller:[function() {
+                
+            }]
+        }
+    }])
