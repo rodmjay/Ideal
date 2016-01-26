d5377ce26e2d646e3868429b73dd2f50df2edaa5
Rod Johnson <rodmjay@wcpro.com>
2016-01-25 21:54:49 -0700

    miscellaneous commit

---
+++ Ideal/Infrastructure/ViewPages/BaseViewPage.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Ideal.Core.Interfaces.Membership;
+using Ideal.Core.Interfaces.Site;
+
+namespace Ideal.Infrastructure.ViewPages
+{
+    public abstract class BaseViewPage<T> : WebViewPage<T>
+    {
+        public ISiteSettings SiteSettings =>
+            DependencyResolver.Current.GetService<ISiteSettings>();
+
+        public IMembershipSettings MembershipSettings =>
+            DependencyResolver.Current.GetService<IMembershipSettings>();
+    }
+
+    public abstract class BaseViewPage : WebViewPage
+    {
+        public ISiteSettings SiteSettings =>
+            DependencyResolver.Current.GetService<ISiteSettings>();
+
+        public IMembershipSettings MembershipSettings =>
+            DependencyResolver.Current.GetService<IMembershipSettings>();
+    }
+}
